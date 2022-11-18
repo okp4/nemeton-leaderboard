492ce4d77699e394a0c47e954c621976bd731282
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-11-18 15:03:03 +0100

    feat(event): implements event store

---
+++ app/event/store.go
@@ -0,0 +1,31 @@
+package event
+
+import (
+	"context"
+
+	"okp4/nemeton-leaderboard/app/util"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const collectionName = "events"
+
+type Store struct {
+	db *mongo.Database
+}
+
+func NewStore(ctx context.Context, mongoURI, dbName string) (*Store, error) {
+	db, err := util.OpenMongoDatabase(ctx, mongoURI, dbName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Store{
+		db: db,
+	}, nil
+}
+
+func (s *Store) Push(ctx context.Context, evt Event) error {
+	_, err := s.db.Collection(collectionName).InsertOne(ctx, evt)
+	return err
+}
