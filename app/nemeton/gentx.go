e6e983d28f1a6af3ad252326b7946a246388e7f0
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-12-02 18:14:10 +0100

    feat(gentx): implements gentx parsing to extract val

---
+++ app/nemeton/gentx.go
@@ -0,0 +1,30 @@
+package nemeton
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+func ParseGenTX(genMap map[string]interface{}) (*types.MsgCreateValidator, error) {
+	raw, err := json.Marshal(genMap)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := simapp.MakeTestEncodingConfig().TxConfig.TxJSONDecoder()(raw)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tx.GetMsgs()) != 1 {
+		return nil, fmt.Errorf("couldn't find 'MsgCreateValidator' in gentx")
+	}
+
+	if msg, ok := tx.GetMsgs()[0].(*types.MsgCreateValidator); ok {
+		return msg, nil
+	}
+	return nil, fmt.Errorf("couldn't find 'MsgCreateValidator' in gentx")
+}
