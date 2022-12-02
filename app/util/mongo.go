7cec83831d978faa7eab86f7b6a1e19d8b881c63
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2022-12-02 18:13:11 +0100

    feat(mongo): add bson codec for cosmos addr

--- app/util/mongo.go
+++ app/util/mongo.go
@@ -3,12 +3,19 @@ package util
 import (
 	"context"
 
+	mongocodec "okp4/nemeton-leaderboard/internal/mongo"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func OpenMongoDatabase(ctx context.Context, mongoURI, dbName string) (*mongo.Database, error) {
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().
+			ApplyURI(mongoURI).
+			SetRegistry(mongocodec.MakeRegistry()),
+	)
 	if err != nil {
 		return nil, err
 	}
---
+++ internal/mongo/codec.go
@@ -0,0 +1,48 @@
+package mongo
+
+import (
+	"reflect"
+
+	"github.com/cosmos/cosmos-sdk/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsonrw"
+)
+
+var (
+	tAccAddress  = reflect.TypeOf(types.AccAddress{})
+	tValAddress  = reflect.TypeOf(types.ValAddress{})
+	tConsAddress = reflect.TypeOf(types.ConsAddress{})
+)
+
+func MakeRegistry() *bsoncodec.Registry {
+	return bson.NewRegistryBuilder().
+		RegisterTypeEncoder(tAccAddress, bsoncodec.ValueEncoderFunc(encodeAccAddress)).
+		RegisterTypeEncoder(tValAddress, bsoncodec.ValueEncoderFunc(encodeValAddress)).
+		RegisterTypeEncoder(tConsAddress, bsoncodec.ValueEncoderFunc(encodeConsAddress)).
+		Build()
+}
+
+func encodeAccAddress(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
+	if !val.IsValid() || val.Type() != tAccAddress {
+		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue", Types: []reflect.Type{tAccAddress}, Received: val}
+	}
+
+	return vw.WriteString(val.Interface().(types.AccAddress).String())
+}
+
+func encodeValAddress(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
+	if !val.IsValid() || val.Type() != tValAddress {
+		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue", Types: []reflect.Type{tValAddress}, Received: val}
+	}
+
+	return vw.WriteString(val.Interface().(types.ValAddress).String())
+}
+
+func encodeConsAddress(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, val reflect.Value) error {
+	if !val.IsValid() || val.Type() != tConsAddress {
+		return bsoncodec.ValueEncoderError{Name: "ObjectIDEncodeValue", Types: []reflect.Type{tConsAddress}, Received: val}
+	}
+
+	return vw.WriteString(val.Interface().(types.ConsAddress).String())
+}
